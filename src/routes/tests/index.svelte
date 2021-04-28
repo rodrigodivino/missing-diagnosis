000d80fcadbdbe71db93e372673c2d5de5b17add
rodrigodivino <rodrigo.sad.lima@gmail.com>
2021-04-28 12:09:35 -0300

    style: Full usability testing structure style adjustments

--- src/routes/tests/index.svelte
+++ src/routes/tests/index.svelte
@@ -21,12 +21,13 @@
         index += 1;
     }
 
-    const decompress = 'woLCsHF1YW50aXRhxIZ2ZUTEiWHCksKFwqbEhHN3ZXLCoWHCq2VsYXBzZWRUaW1lw4s_w698w63CkWhywrDComlkwqRRMS1pwqVncm91cMKnbnVsbMSHecKmdcSfcklEwrIwLjc2NTk3NzAzNjIxNDAwxY_EkcSTxJXEl2TEmsScxJ7EoMSixKTEpsO2LQ5WBBjCk8SvZMKlxLPEtcS2xLjEusS8xL7FgMWCxYTFhsWIxYrFjMWOxZDFksWUxZbFmMWaOcKvxIJhxYHEiMSKxIzEjsKawoLFr8Kkc8WFMcSSbsSUxJbCr3N0xLluZ19kaXNhxLhlZcaRxLDGk8WFMsaXxplyxpvGnW_Gn8ahxqPGpXLGp8apxLHGlHMzxq7FnsKoxqLGpMamxqjGksa8NMa_xJbHgca2x4TGusarczXHiXLHi8eDxrjHhcaqxrw2x5LGscaexqDHgsa3xrnHhsWFN8eSx5THoMeXxrvFhTjHpcefx43HonM5x5vGnMedxrXHlcehxLDCpca8MTDHrMeMx5Y-x7_FsMiBx4PHpMiFx6bGpceJx4vIjMarP8O0w7XDgsKPXCjDtse2yJfGuMe5x5vGvMe9x5_InseCx6PIhMeGx6fGpsimxqrHjsOxxrHDt8OOw5kXx5Y1yJjHusecxr3Hvsaax6HIgseEx6XIh8eoyIrFk8inx47JhcWaxZwCx7bJisizx5rGu8e8x57Gv8i5x6LIg8eFyIbHh8eoxqfJmMmBx67HsMeyx7THljbJi8i1yaTInMmQyJ_Iu8mqyKPIiMmXxqnHjcaEw7XHkceTx5XEocWiybbJociZyY3It8mmx4HJqMmTyKLJlci_yIvJsMSXw7QxJsSaw5TDvseWN8m3yaPIm8mPyIDIusmpyZTJrMWQyoHHrMiow7JeNT_IkMKRx7bKocqMyYzItsmlyJ3KkcmSyKHFiw-dsOJxpBRNMaTxanHvMa-x6HIgMWzyILHhceoyIbHiciJx4zFlsiMxq4_w7V4w5TDvcOzwrZGx5nImMe6yJrGvcegx7_Hgcifx4THp8iFx4jHq8iJx67Hjciox5DDuiTDnciQyJLIljXImca8x5_HvseAx6PIusemyITHh8eqyIjFksaByYLGrceQw7ZNw5LDscKpw7vDp8eZyYvItMmNx73Gv8eixp3HpciDx4bHqciHxqjGgMiLyZvGh8O2FHrDoUfCrhTIljbJjMeeyajInci5x4PJk8muyKPIv8apybPHj8aHw7PGtMaxxrPHmcm-yabKgMicyLjJkcqEya3Iosi-yZfEiMaqyKfJtMSXw6Z2xorGjBDIljfJv8ibyLfJkMmryKDIvMmVybDHrMqexqzKi8SXw63CocOKw4DCgxJvx5nKqMqTyqrJj8mqyIHIu8mUya8'
-    codec.decompress(decompress).then(result => {
-        console.log('resultado: ', result)
-    })
+    // const decompress = 'woPCsHF1YW50aXRhxIZ2ZUTEiWHCksKFwqbEhHN3ZXLCoWHCq2VsYXBzZWRUaW1lw4s_w7vCvnbDiMK0OVjComlkwqRRMS1pwqVncm91cMKnbnVsbMSHecKmdcSfcklEwrIwLjY4MzAxOTIyMzM2NTQwNTPEkcSTxJXEl2TEmsScxJ7EoMSixKTEpsOzGsKfxKnEq8SvZMKlxLPEtcS2xLjEusS8xL7FgMWCxYTFhsWIxYrFjMWOxZDFksWUxZbFmMWaM8KvxIJhxYHEiMSKxIzEjsKawoLFrsKkc8WFMcSSbsSUxJbCr3N0xLluZ19kaXNhxLhlZcaQxLDGksWFMsaWxphyxprGnG_GnsagxqLGpHLGpsaoxLHGk3Mzxq3Fn8awxp3Gn8ahxqPGpcanxpHGuzTGvsaZxpvHgca0x4TGt8eGxqnGuzXHisavx4zGsseCxrXHhca5xqpzNseVx4DHmMeOxrbGuMeHxYU3x6DHl8azx4PHpMeRxrrFhTjHqcaxx6vHmseQx5zGuznHsseNx6zHm8WuwqXGuzEwx7rHose8x5DCrWTEiWVDb21wbGV0xKDCszI4LzA0LzIwMjEgMTI6yJs6NTU-x7_FsMiBx4PHpMiFx6bGpceJx4vIjMarP8O0w7XDgsKPXCjDtse2yJfGuMe5x5vGvMe9x5_InseCx6PIhMeGx6fGpsimxqrHjsOxxrHDt8OOw5kXx5Y1yJjHusecxr3Hvsaax6HIgseEx6XIh8eoyIrFk8inx47JhcWaxZwCx7bJisizx5rGu8e8x57Gv8i5x6LIg8eFyIbHh8eoxqfJmMmBx67HsMeyx7THljbJi8i1yaTInMmQyJ_Iu8mqyKPIiMmXxqnHjcaEw7XHkceTx5XEocWiybbJociZyY3It8mmx4HJqMmTyKLJlci_yIvJsMSXw7QxJsSaw5TDvseWN8m3yaPIm8mPyIDIusmpyZTJrMWQyoHHrMiow7JeNT_IkMKRx7bKocqMyYzItsmlyJ3KkcmSyKHFiw-dsOJxpBRNMaTxanHvMa-x6HIgMWzyILHhceoyIbHiciJx4zFlsiMxq4_w7V4w5TDvcOzwrZGx5nImMe6yJrGvcegx7_Hgcifx4THp8iFx4jHq8iJx67Hjciox5DDuiTDnciQyJLIljXImca8x5_HvseAx6PIusemyITHh8eqyIjFksaByYLGrceQw7ZNw5LDscKpw7vDp8eZyYvItMmNx73Gv8eixp3HpciDx4bHqciHxqjGgMiLyZvGh8O2FHrDoUfCrhTIljbJjMeeyajInci5x4PJk8muyKPIv8apybPHj8aHw7PGtMaxxrPHmcm-yabKgMicyLjJkcqEya3Iosi-yZfEiMaqyKfJtMSXw6Z2xorGjBDIljfJv8ibyLfJkMmryKDIvMmVybDHrMqexqzKi8SXw63CocOKw4DCgxJvx5nKqMqTyqrJj8mqyIHIu8mUya8'
+    // codec.decompress(decompress).then(result => {
+    //     console.log('resultado: ', result)
+    // })
     $: if (quantitativeComplete) {
-        codec.compress({quantitativeData, qualitativeData}).then(result => {
+        const dateCompleted = new Date().toLocaleString();
+        codec.compress({quantitativeData, qualitativeData, dateCompleted}).then(result => {
             compressedResult = result;
         });
     }
@@ -57,6 +58,10 @@
         }
     }
 
+    let emailSent = null;
+    function handleEmailClick() {
+        emailSent = true;
+    }
     let qualitativeForm = null;
 </script>
 
@@ -123,10 +128,19 @@
     }
 
     button.qualitative-finish {
-        margin-bottom: 10px;
+        margin-bottom: 50px;
     }
 
+    button.copy-button {
+        margin-top: 5px;
+        bottom: 10px;
+    }
     form .likert li label {width:100%;}
+
+    button.email-confirmation {
+        margin: 10px auto;
+        max-width: 300px;
+    }
 </style>
 
 <svelte:head>
@@ -138,11 +152,17 @@
         {#if compressedResult}
             <div class="final-flex">
                 <h1> Para finalizar:</h1>
-                <h2> Envie-me o seguinte código pelo e-mail "rodrigodivino.ufpa@gmail.com" com o assunto "Teste de
+                <h2> Envie o seguinte código para e-mail "rodrigodivino.ufpa@gmail.com" com o assunto "Teste de
                     Usabilidade"</h2>
                 <label for="result"></label>
                 <textarea bind:this={resultTextArea} readonly id='result'>{compressedResult}</textarea>
-                <button on:click={handleCopyClick}>Copiar para Área de Transferência</button>
+                <button class="copy-button" on:click={handleCopyClick}>Copiar para Área de Transferência</button>
+
+                <button class="email-confirmation" on:click={handleEmailClick}>Ok, já enviei o e-mail.</button>
+
+                {#if emailSent}
+                    <h1> Fim do teste. Muito obrigado por sua participação! </h1>
+                    {/if}
             </div>
         {:else}
             <h3> O seu resultado está sendo gerado. Aguarde...</h3>
